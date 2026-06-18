885afcca6c9c8cb0582f121c27c08c6be1cd1ddc
Muhammad Ikram <ikramdinata@gmail.com>
2026-06-18 18:34:45 +0800

    fix: restore opening style tag for Select2 styles in deliveries create view

--- resources/views/communication/deliveries/create.blade.php
+++ resources/views/communication/deliveries/create.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <!-- Select2 CSS & JS Assets -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<style>
     .select2-container--default .select2-selection--single {
         height: 42px !important;
         border: 1px solid rgb(209, 213, 219) !important;
